685d38cdbda1d860c3a780ec1a9b929490c22073
Mapachana <anabuenrua99@gmail.com>
2019-02-21 18:00:32 +0100

    Añado ShieldBooster en java y ruby y corrijo pequeño fallito de SuppliesPackage en ruby

--- DeepSpace-Java/src/deepspace/ShieldBooster.java
+++ DeepSpace-Java/src/deepspace/ShieldBooster.java
@@ -1,1 +1,36 @@
 package deepspace;
+
+class ShieldBooster{
+   private String name;
+   private float boost;
+   private int uses;
+
+   ShieldBooster(String n, float b, int u) {
+      name = n;
+      boost = b;
+      uses = u;
+   }
+
+   ShieldBooster(ShieldBooster s) {
+      name = s.name;
+      boost = s.boost;
+      uses = s.uses;
+   }
+
+   public float getBoost() {
+      return boost;
+   }
+
+   public int getUses() {
+      return uses;
+   }
+
+   public float uselt() {
+      if (uses > 0){
+         uses--;
+         return boost;
+      }
+      else
+      return 1.0f;
+   }
+}
---
+++ DeepSpace-Ruby/lib/ShieldBooster.rb
@@ -0,0 +1,35 @@
+#encoding: UTF-8
+module DeepSpace
+   class ShieldBooster
+      def initialize (n, b, u)
+         @name = n
+         @boost = b
+         @uses = u
+      end
+
+      def nexCopy (s)
+         @name = s.name()
+         @boost = s.boost()
+         @uses = s.uses()
+      end
+
+      def boost
+         return @boost
+      end
+
+      def uses
+         return @uses
+      end
+
+      def uselt
+         if (@uses > 0)
+            @uses -= 1
+            return @boost
+         else
+            return 1.0
+         end
+      end
+
+   end
+
+end
