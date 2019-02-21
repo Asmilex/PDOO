ee13e04355cf4f4cdf289057defe598466817187
Asmilex <andresmm@outlook.com>
2019-02-21 13:42:51 +0100

    Añadido WeaponType

--- DeepSpace-Java/src/deepspace/WeaponType.java
+++ DeepSpace-Java/src/deepspace/WeaponType.java
@@ -1,9 +1,9 @@
 package deepspace;
 
 public enum WeaponType {
-    LASER(2.0),
-    MISSILE(3.0),
-    PLASMA(4.0);
+    LASER(2.0f),
+    MISSILE(3.0f),
+    PLASMA(4.0f);
 
     private float power;
 
