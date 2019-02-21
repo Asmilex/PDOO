5df99c26ff6a288e4aa736b19080882222bb7b2c
Mapachana <anabuenrua99@gmail.com>
2019-02-21 14:03:57 +0100

    Añado SuppliesPackage en java y ruby

--- DeepSpace-Java/src/deepspace/SuppliesPackage.java
+++ DeepSpace-Java/src/deepspace/SuppliesPackage.java
@@ -1,1 +1,32 @@
 package deepspace;
+
+class SuppliesPackage{
+   private float ammoPower;
+   private float fuelUnits;
+   private float shieldPower;
+
+   SuppliesPackage(float a,float f,float s){
+      ammoPower = a;
+      fuelUnits = f;
+      shieldPower = s;
+   }
+
+   SuppliesPackage(SuppliesPackage s){
+      ammoPower = s.getammoPower();
+      fuelUnits = s.getfuelUnits();
+      shieldPower = s.getshieldPower();
+   }
+
+   public float getammoPower(){
+      return ammoPower;
+   }
+
+   public float getfuelUnits(){
+      return fuelUnits;
+   }
+
+   public float getshieldPower(){
+      return shieldPower;
+   }
+
+}
---
+++ DeepSpace-Ruby/lib/SuppliesPackage.rb
@@ -0,0 +1,33 @@
+#encoding UTF-8
+
+module DeepSpace
+   class SuppliesPackage
+      
+      def initialize(a,f,s)
+         @ammoPower = a
+         @fuelUnits = f
+         @shieldPower = s
+      end
+   
+      def newCopy(s)
+         @ammoPower = s.ammoPower()
+         @fuelUnits = s.fuelUnits()
+         @shieldPower = s.shieldPower()
+      end
+
+      def ammoPower
+         return @ammoPower
+      end
+
+      def fuelUnits
+         return @fuelUnits
+      end
+
+      def shieldPower
+         return @shieldPower
+      end
+
+   end
+end
+
+
