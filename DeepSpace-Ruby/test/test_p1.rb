60a1ff3644c0641ff7397f40b77f3309bf7d1cfe
Asmilex <andresmm@outlook.com>
2019-02-27 13:23:42 +0100

    Cambios en el main de Ruby. Se muestran las probabiliades en dice

--- DeepSpace-Ruby/test/test_p1.rb
+++ DeepSpace-Ruby/test/test_p1.rb
@@ -11,45 +11,69 @@ require '../lib/Weapon.rb'
 require '../lib/WeaponType.rb'
 
 module DeepSpace
-   class TestP1
-      def initialize
+    class TestP1
+        def initialize
 
-      end
+        end
 
-      def self.main
+        def self.main
 
-         botin = Loot.new(1,2,3,4,5)
-         puts "Prueba de interfaces de Loot:"
-         puts "-> Supplies: #{botin.nSupplies}\n-> NWeapons: #{botin.nWeapons}\n-> NShields: #{botin.nShields}\n-> Hangars: #{botin.nHangars}\n-> Medals: #{botin.nMedals}\n\n"
+            botin = Loot.new(1,2,3,4,5)
+            puts "Prueba de interfaces de Loot:"
+            puts "-> Supplies: #{botin.nSupplies}\n-> NWeapons: #{botin.nWeapons}\n-> NShields: #{botin.nShields}\n-> Hangars: #{botin.nHangars}\n-> Medals: #{botin.nMedals}\n\n"
 
-         arma = Weapon.new("Rayo", WeaponType::LASER, 1)
-         puts "Prueba de métodos de Weapon:"
-         puts "-> Tipo: #{arma.type}\n-> Usos: #{arma.uses}\n->Potencia: #{arma.power}"
-         arma.useIt
-         puts "Tras useIt tiene estos usos: #{arma.uses}\n\n"
+            arma = Weapon.new("Rayo", WeaponType::LASER, 1)
+            puts "Prueba de métodos de Weapon:"
+            puts "-> Tipo: #{arma.type}\n-> Usos: #{arma.uses}\n->Potencia: #{arma.power}"
+            arma.useIt
+            puts "Tras useIt tiene estos usos: #{arma.uses}\n\n"
 
-         escudo = ShieldBooster.new("Ligero", 3.5, 0)
-         puts "Prueba de métodos de ShieldBooster:"
-         puts "-> Boost: #{escudo.boost}\n-> Usos: #{escudo.uses}\n\n"
+            escudo = ShieldBooster.new("Ligero", 3.5, 0)
+            puts "Prueba de métodos de ShieldBooster:"
+            puts "-> Boost: #{escudo.boost}\n-> Usos: #{escudo.uses}\n\n"
 
-         suministros = SuppliesPackage.new(3.0,50,3.5)
-         puts "Prueba de métodos de SuppliesPackage"
-         puts "-> Ammo: #{suministros.ammoPower}\n-> Fuel: #{suministros.fuelUnits}\n-> Potencia: #{suministros.shieldPower}\n\n"
+            suministros = SuppliesPackage.new(3.0,50,3.5)
+            puts "Prueba de métodos de SuppliesPackage"
+            puts "-> Ammo: #{suministros.ammoPower}\n-> Fuel: #{suministros.fuelUnits}\n-> Potencia: #{suministros.shieldPower}\n\n"
 
-         dado = Dice.new()
-         jugadores = 5
+            dado = Dice.new()
+            jugadores = 5
 
-         puts "Prueba de métodos de Dice"
-         puts "Jugadores: #{jugadores}"
-         puts "Primer disparo: #{dado.firstShot}"
-         puts "Hacemos 5 iteraciones para ver la generación de números:"
+            puts "Prueba de métodos de Dice"
+            puts "Jugadores: #{jugadores}"
+            puts "Hacemos 100 iteraciones para ver la generación de números aleatorios:"
 
-         for i in 1..5
-            puts "Iteración #{i}:"
-            puts "-> Hangares: #{dado.initWithNHangars}\n-> Armas: #{dado.initWithNWeapons}\n-> Escudos: #{dado.initWithNShields}\n-> Mueve: #{dado.spaceStationMoves(0.5)}\n-> Empieza: #{dado.whoStarts(jugadores)}\n\n"
-         end
+            cuenta_hangares      = [0,0]
+            cuenta_escudos       = [0,0]
+            cuenta_armas         = [0,0,0]
+            cuenta_quien_dispara = [0,0]
+            cuenta_quien_empieza = Array.new(jugadores, 0)
 
-      end
+            for i in 1..100
+                cuenta_hangares[dado.initWithNHangars]   += 1
+                cuenta_escudos[dado.initWithNShields]    += 1
+                cuenta_armas[dado.initWithNWeapons - 1]  += 1
+                cuenta_quien_empieza[dado.whoStarts(jugadores) - 1] += 1
+
+                if dado.spaceStationMoves(0.5) == GameCharacter::ENEMYSTARSHIP
+                    cuenta_quien_dispara[0] += 1
+                else
+                    cuenta_quien_dispara[1] += 1
+                end
+
+            end
+
+            puts "Hangares: 0 -> #{cuenta_hangares[0]}, 0 -> #{cuenta_hangares[1]}"
+            puts "Escudos: 0 -> #{cuenta_escudos[0]}, 0 -> #{cuenta_escudos[1]}"
+            puts "Armas: 1 -> #{cuenta_armas[0]}, 2 -> #{cuenta_armas[1]}, 3 -> #{cuenta_armas[2]}"
+            puts "Quién mueve: ENEMYSTARSHIP #{cuenta_quien_dispara[0]}, SPACESTATION #{cuenta_quien_dispara[1]}"
+            puts "Qué jugador empieza:"
+
+            for i in 0...jugadores
+                puts "#{i + 1}: #{cuenta_quien_empieza[i]} veces"
+            end
+
+        end
    end
 
    TestP1.main
