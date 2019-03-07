be33bbea3f8991b4b98919b038cb76a6ce0200c8
Asmilex <andresmm@outlook.com>
2019-03-07 14:21:04 +0100

    Implementados métodos de Hangar

--- DeepSpace-Java/src/deepspace/Hangar.java
+++ DeepSpace-Java/src/deepspace/Hangar.java
@@ -1,5 +1,80 @@
 package deepspace;
 
-class Hangar {
+import java.util.ArrayList;
 
+class Hangar {
+    private int maxElements;
+    private ArrayList<Weapon> weapons        = new ArrayList<>();
+    private ArrayList<ShieldBooster> shields = new ArrayList<>();
+
+
+    Hangar (int capacity) {
+        maxElements = capacity;
+    }
+
+    Hangar (Hangar h) {
+        maxElements = h;
+    }
+
+//
+// ──────────────────────────────────────────────────────────── MODIFICADORES ─────
+//
+
+
+    public boolean addWeapon (Weapon w) {
+        if (SpaceAvailable()) {
+            weapons.add(w);
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    public boolean addShieldBoosters (ShieldBooster s) {
+        if (SpaceAvailable()) {
+            shields.add(s);
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+//
+// ────────────────────────────────────────────────────────────── CONSULTORES ─────
+//
+
+    public int getMaxElements () {
+        return maxElements;
+    }
+
+    public ArrayList<ShieldBooster> getShieldBoosters () {
+        return shields;
+    }
+
+    public ArrayList<Weapon> getWeapons() {
+        return weapons;
+    }
+
+    private boolean SpaceAvailable () {
+        // NOTE se refiere al tamaño entre ambos o al individual?
+    }
+
+//
+// ─────────────────────────────────────────────────────────────────── REMOVE ─────
+//
+
+    public ShieldBooster removeShieldBooster (int s) {
+        return shields.remove(s);
+    }
+
+    public Weapon removeWeapon (int w) {
+        return weapons.remove(w);
+    }
+
+
+    HangarToUI getUIversion () {
+        return new HangarToUI(this);
+    }
 }
