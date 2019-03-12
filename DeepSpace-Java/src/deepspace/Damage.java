dd65484ab39adea6a8681b5fd03b3b082b1ca2c2
Asmilex <andresmm@outlook.com>
2019-03-12 21:07:27 +0100

    Implementados algunos métodos de Damage.java

--- DeepSpace-Java/src/deepspace/Damage.java
+++ DeepSpace-Java/src/deepspace/Damage.java
@@ -1,16 +1,23 @@
 package deepspace;
 
+import java.util.ArrayList;
+
+// NOTE Convendría volver a repasar el código una vez sepamos qué tiene que hacer exactamente
+// Ahora mismo voy un poco a ciegas
+
 class Damage {
     private int nShields;
     private int nWeapons;
+    private ArrayList<Weapon> weapons = new ArrayList<>();
 
     Damage (int w, int s) {
         nShields = s;
         nWeapons = w;
     }
 
-    Damage (WeaponType[] wl, int s) {
-
+    Damage (ArrayList<Weapon> wl, int s) {
+        weapons = wl;
+        nShields = s;
     }
 
     Damage (Damage d) {
@@ -22,25 +29,36 @@ class Damage {
         return new DamageToUI(this);
     }
 
-    private int arrayContainsType (Weapon[] w, WeaponType t) {
+    private int arrayContainsType (ArrayList<Weapon> w, WeaponType t) {
+        for (int i = 0; i < w.size(); i++) {
+            if (w.get(i).getType() == t)
+                return i;
+        }
 
+        return -1;
     }
 
-    
-    public Damage adjust (Weapon[] w, ShieldBooster[] s) {
 
+    public Damage adjust (ArrayList<Weapon> w, ArrayList<ShieldBooster> s) {
+        //TODO
     }
 
     public void discardWeapon (Weapon w) {
-
+        if (weapons.size() != 0) {
+            weapons.remove(w);
+        }
+        else if (nWeapons > 0){
+            nWeapons--;
+        }
     }
 
     public void discardShieldBooster () {
-
+        if (nShields > 0)
+            nShields--;
     }
 
     public boolean hasNoEffect () {
-
+        // TODO
     }
 
     public int getNShields () {
@@ -51,8 +69,8 @@ class Damage {
         return nWeapons;
     }
 
-    public WeaponType[] getWeapons () {
-
+    public ArrayList<Weapon> getWeapons () {
+        return weapons;
     }
 
 }
