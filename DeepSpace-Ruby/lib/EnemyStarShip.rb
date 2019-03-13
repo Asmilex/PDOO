bdb217b3e3339a7a4eded5460c352a53504553a5
Asmilex <andresmm@outlook.com>
2019-03-13 18:41:20 +0100

    Implementada clase EnemyStarShip.rb

--- DeepSpace-Ruby/lib/EnemyStarShip.rb
+++ DeepSpace-Ruby/lib/EnemyStarShip.rb
@@ -2,4 +2,46 @@
 
 class EnemyStarShip
 
+#
+# ──────────────────────────────────────────────────────────── CONSTRUCTORES ─────
+#
+
+    def initialize (n, a, s, l, d)
+        @name        = n;
+        @loot        = l;
+        @damage      = d;
+        @ammoPower   = a;
+        @shieldPower = s;
+    end
+
+    def self.newCopy(s)
+        new (s.name, s.loot, s.damage, s.ammoPower, s.shieldPower)
+    end
+
+
+    def getUIversion()
+        new EnemyToUI.new(self)
+    end
+
+#
+# ─────────────────────────────────────────────────────────────── INTERFACES ─────
+#
+
+    def protection
+        @shieldPower
+    end
+
+    def fire
+        @ammoPower
+    end
+
+    def receiveShot (shot)
+        if shot < @shieldPower
+            ShotResult::DONOTRESIST
+        else
+            ShotResult::RESIST
+        end
+    end
+
+    attr_reader :name, :loot, :damgage, :ammoPower, :shieldPower
 end
