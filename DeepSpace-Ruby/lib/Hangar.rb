c249e8617b23b4ef5deecb3b17045009b1ad7eb1
Asmilex <andresmm@outlook.com>
2019-03-14 10:38:04 +0100

    Implementados métodos de la clase Hangar.ruby

--- DeepSpace-Ruby/lib/Hangar.rb
+++ DeepSpace-Ruby/lib/Hangar.rb
@@ -1,5 +1,54 @@
 # encoding:utf-8
 
 class Hangar
+    attr_reader :maxElements, :weapons, :shields
 
+    def initialize (capacity)
+        @maxElements = capacity
+        @weapons     = Array.new
+        @shields     = Array.new
+    end
+
+    def self.newCopy (h)
+        auxiliar = new(h.maxElements)
+
+        h.shields.each { |escudos| auxiliar.addShieldBoosers(escudos) }
+        h.weapons.each { | armas | auxiliar.addWeapon(armas)}
+
+        auxiliar
+    end
+
+    def SpaceAvaliable()
+        @shields.size + @weapons.size < @maxElements
+    end
+
+    def addWeapon (w)
+        if self.SpaceAvaliable
+            @weapons.push(w)
+            true
+        end
+
+        false
+    end
+
+    def addShieldBoosers (s)
+        if SpaceAvaliable()
+            @shields.push(s)
+            true
+        end
+
+        false
+    end
+
+    def removeShieldBooster (s)
+        @shields.delete(s)
+    end
+
+    def removeWeapon (w)
+        @weapons.delete(w)
+    end
+
+    def getUIversion
+        HangarToUI.new(self)
+    end
 end
