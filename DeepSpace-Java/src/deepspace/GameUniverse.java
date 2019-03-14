0af0a44b3cf2b01d529165aa2dca925a5966beb0
Mapachana <anabuenrua99@gmail.com>
2019-03-14 14:21:19 +0100

    Añado gameuniverse en java y spacestation en ruby a medias

--- DeepSpace-Java/src/deepspace/GameUniverse.java
+++ DeepSpace-Java/src/deepspace/GameUniverse.java
@@ -1,5 +1,55 @@
 package deepspace;
 
 class GameUniverse {
+   private static int WIN = 10;
+   private int currentStationIndex;
+   private int turns;
+   private GameStateController gameState;
+   private Dice dice;
+   private EnemyStarShip currentEnemy;
+   private SpaceStation currentStation;
+   private ArrayList<SpaceStation> spaceStations = new ArrayList<>();
 
+   GameUniverse(){ // FIXME no se si es asi
+      gameState = new GameStateController;
+      dice = new Dice;
+      turns = 0;
+   }
+
+   public boolean haveAWinner(){
+      if (currentStation.getNMedals() == WIN)
+         return true;
+      else
+         return false;
+   }
+   
+   public void mountShieldBooster(int i){
+      if (gameState.state == GameState.INIT or gameState.state == GameState.AFTERCOMBAT) 
+         currentStation.mountShieldBooster(i);
+   }
+
+   public void mountWeapon(int i){
+      if (gameState.state == GameState.INIT or gameState.state == GameState.AFTERCOMBAT)  
+         currentStation.mountWeapon(i);
+   }
+
+   public void discardShieldBooster(int i){
+      if (gameState.state == GameState.INIT or gameState.state == GameState.AFTERCOMBAT)
+         currentStation.discardShieldBooster(i);
+   }
+
+   public void discardWeapon(int i){
+      if (gameState.state == GameState.INIT or gameState.state == GameState.AFTERCOMBAT)
+         currentStation.discardWeapon(i);
+   }
+
+   public void discardShieldBoosterInHangar(int i){
+      if (gameState.state == GameState.INIT or gameState.state == GameState.AFTERCOMBAT) 
+         currentStation.discardShieldBoosterInHangar(i);
+   }
+
+   public void discardWeaponInHangar(int i){
+      if (gameState.state == GameState.INIT or gameState.state == GameState.AFTERCOMBAT)
+         currentStation.discardWeaponInHangar(i);
+   }
 }
--- DeepSpace-Ruby/lib/SpaceStation.rb
+++ DeepSpace-Ruby/lib/SpaceStation.rb
@@ -1,5 +1,32 @@
 # encoding:utf-8
 
 class SpaceStation
+   @@MAXFUEL = 100
+   @@SHIELDLOSSPERUNITSHOT = 0.1
+   
+   def initialize
+      @ammoPower
+      @fuelUnits
+      @name
+      @nMedals
+      @shieldPower
+      @pendingDamage #array
+      @weapons #array
+      @shieldBoosters #array
+      @hangar #array
+   end
 
+   def assignFuelValue (f)
+      @fuelUnits = Math.max(@@MAXFUEL,f)
+   end
+   
+   def cleanPendingDamage()
+      if(@pendingDamage.hasNoEffect)
+         @pendingDamage = null
+   end
+
+   def receiveWeapon(Weapon w)
+   # ESTO FALTA POR HACERLOOOOOOOOOOOOOOOOOOOOOOOOOOOOO  
+   end
+      
 end
