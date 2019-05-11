a83ab3728fee2e1bfcbaef01beed59c7762a5b84
Asmilex <andresmm@outlook.com>
2019-05-11 12:51:00 +0200

    Implementada clase PowerEfficientSpaceStation

---
+++ DeepSpace-Java/src/deepspace/PowerEfficientSpaceStation.java
@@ -0,0 +1,32 @@
+package deepspace;
+
+class PowerEfficientSpaceStation extends SpaceStation {
+    private float EFFICIENCYFACTOR = 1.10f;
+
+    PowerEfficientSpaceStation (SpaceStation estacion) {
+        super(estacion);
+    }
+
+    @Override
+    public float fire () {
+        return super.fire() * EFFICIENCYFACTOR;
+    }
+
+    @Override
+    public float protection () {
+        return super.protection() * EFFICIENCYFACTOR;
+    }
+
+    public Transformation setLoot (Loot loot) {
+        super.setLoot(loot);
+
+        return loot.getEfficient()
+            ?   Transformation.GETEFFICIENT
+            :   Transformation.NOTRANSFORM;
+    }
+
+    @Override
+    public String toString() {
+        return "Power efficient Space Station:\n" + super.toString();
+    }
+}
