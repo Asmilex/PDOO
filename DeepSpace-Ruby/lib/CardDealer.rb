c6d4f98da13e5fe1a4f62838fa03f4f80283fb79
Mapachana <anabuenrua99@gmail.com>
2019-05-29 22:59:29 +0200

    Hago que jueguen todos los enemigos en ruby

--- DeepSpace-Ruby/lib/CardDealer.rb
+++ DeepSpace-Ruby/lib/CardDealer.rb
@@ -153,7 +153,7 @@ class CardDealer
         softSpecificDamage1   = SpecificDamage.new([WeaponType::MISSILE],1)
         softSpecificDamage2   = SpecificDamage.new([WeaponType::PLASMA],1)
         mediumSpecificDamage0 = SpecificDamage.new([WeaponType::LASER,WeaponType::MISSILE],2)
-=begin
+
         @enemies.add(EnemyStarShip.new("Enemigo fácil -1",0,0,regularLoot0,lowDamage0));
         @enemies.add(EnemyStarShip.new("Enemigo fácil -1",0,10,badLoot,lowDamage0));
         @enemies.add(EnemyStarShip.new("Enemigo fácil -1",10,0,badLoot,lowDamage0));
@@ -184,16 +184,17 @@ class CardDealer
         @enemies.add(EnemyStarShip.new("Enemigo imposible",500,500,goodLoot,hardSpecificDamage0));
 
         # Enemigos que transforman
-=end
+
         transformLoot0 = Loot.new(1,1,1,0,1,true,false)
         transformLoot1 = Loot.new(1,1,1,0,1,false,true)
-=begin
+
         @enemies.add(EnemyStarShip.new("Enemigo transforma 0",200,100,transformLoot0,regularDamage0))
         @enemies.add(EnemyStarShip.new("Enemigo transforma 1",100,200,transformLoot1,regularDamage1))
-=end
 
+=begin
         @enemies.add(EnemyStarShip.new("Enemigo transforma 0",0,0,transformLoot0,regularDamage0))
         @enemies.add(EnemyStarShip.new("Enemigo transforma 1",0,0,transformLoot1,regularDamage1))
+=end
     end
 end # class
 
